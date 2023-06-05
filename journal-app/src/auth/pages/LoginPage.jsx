7a10c2cc233b7d9d1b75b0084252d379058c298d
Analia Lujan Zunino <analia.zunino@accenture.com>
2023-06-05 16:22:00 -0300

    login page

--- journal-app/src/auth/pages/LoginPage.jsx
+++ journal-app/src/auth/pages/LoginPage.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import { Link as RouterLink } from 'react-router-dom';
+import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { Google } from '@mui/icons-material';
+import { AuthLayout } from '../layout/AuthLayout';
+
 
 export const LoginPage = () => {
   return (
-    <div>
-      login
-    </div>
+   <AuthLayout title="Login">
+      <form>
+          <Grid container>
+            <Grid item xs={ 12 } sx={{ mt: 2 }}>
+              <TextField 
+                label="Correo" 
+                type="email" 
+                placeholder='correo@google.com' 
+                fullWidth
+              />
+            </Grid>
+
+            <Grid item xs={ 12 } sx={{ mt: 2 }}>
+              <TextField 
+                label="Contraseña" 
+                type="password" 
+                placeholder='Contraseña' 
+                fullWidth
+              />
+            </Grid>
+            
+            <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
+              <Grid item xs={ 12 } sm={ 6 }>
+                <Button variant='contained' fullWidth>
+                  Login
+                </Button>
+              </Grid>
+              <Grid item xs={ 12 } sm={ 6 }>
+                <Button variant='contained' fullWidth>
+                  <Google />
+                  <Typography sx={{ ml: 1 }}>Google</Typography>
+                </Button>
+              </Grid>
+            </Grid>
+
+
+            <Grid container direction='row' justifyContent='end'>
+              <Typography sx={{ mr: 1 }}>¿ Ya tienes cuenta ?</Typography>
+              <Link component={ RouterLink } color='inherit' to="/auth/register">
+                Crear Cuenta
+              </Link>
+            </Grid>
+
+          </Grid>
+
+
+        </form>
+
+    </AuthLayout>
   )
 }
-
-
